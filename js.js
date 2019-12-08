d940f8700b00fc68e3960cbb00c63c160f0ef41f
Lukas <Mechaniczny7@gmail.com>
2019-12-08 15:53:10 +0100

    04

--- js.js
+++ js.js
@@ -1,1 +1,9 @@
 console.log("plik zewnętrzny");
+function init(){
+document.querySelectorAll(".school-title").forEach(item => item.addEventListener("click",showDescription ));
+}
+function showDescription(event){
+const parent = event.path[1];
+parent.classList.toggle("active");
+}
+window.addEventListener("load", init);
