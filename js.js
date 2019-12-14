82b2eda5a8647ac94e3e8e0c284eee0b6855dd6d
Lukas <Mechaniczny7@gmail.com>
2019-12-14 11:57:00 +0100

    nowy02

--- js.js
+++ js.js
@@ -1,11 +1,20 @@
 function init() {
 	document.querySelectorAll('.school-title').forEach(item => item.addEventListener('click', showDescription));
+	document.querySelectorAll(".menu-element").forEach(item => item.addEventListener("click", scrollToElement));
+	document.querySelector("#"+event.target.dataset.name).scrollIntoView({
+		behavior: "smooth"
+	});
 }
 
 function showDescription(event) {
 	const parent = event.target.parentElement;
 	parent.classList.toggle('active');
 }
-
+function scrollToElement(event) {
+    const element = document.querySelector('#'+event.target.dataset.name);
+    if (!element) return;
+    element.scrollIntoView({ 
+    behavior: 'smooth',
+});
+}
 window.addEventListener('load', init);
-
